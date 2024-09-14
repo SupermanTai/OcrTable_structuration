533176e4de74bee9e12763823665711feb61640d
SupermanTai <90685924+SupermanTai@users.noreply.github.com>
2024-09-14 16:09:55 +0800

    Add files via upload

--- table_structure_recognition/mmseg/utils/__init__.py
+++ table_structure_recognition/mmseg/utils/__init__.py
@@ -1,10 +1,16 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .collect_env import collect_env
-from .logger import get_root_logger
-from .misc import find_latest_checkpoint
-from .set_env import setup_multi_processes
+from .embed import PatchEmbed
+from .inverted_residual import InvertedResidual, InvertedResidualV3
+from .make_divisible import make_divisible
+from .res_layer import ResLayer
+from .se_layer import SELayer
+from .self_attention_block import SelfAttentionBlock
+from .shape_convert import (nchw2nlc2nchw, nchw_to_nlc, nlc2nchw2nlc,
+                            nlc_to_nchw)
+from .up_conv_block import UpConvBlock
 
 __all__ = [
-    'get_root_logger', 'collect_env', 'find_latest_checkpoint',
-    'setup_multi_processes'
+    'ResLayer', 'SelfAttentionBlock', 'make_divisible', 'InvertedResidual',
+    'UpConvBlock', 'InvertedResidualV3', 'SELayer', 'PatchEmbed',
+    'nchw_to_nlc', 'nlc_to_nchw', 'nchw2nlc2nchw', 'nlc2nchw2nlc'
 ]
